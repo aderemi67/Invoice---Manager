58531075bd94420f8a2fd80a70285a67833c4e23
Aderemi67 <aderemiazeez6@gmail.com>
2026-05-09 22:09:40 +0100

    Setup Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+function App() {
+  return (
+    <div className="p-5">
+      <h1 className=" text-white
+       bg-black min-h-screen flex items-center
+       justify-center">Invoice Manager </h1>
+      </div>
+  );
+}
+
+export default App;
